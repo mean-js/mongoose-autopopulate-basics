67a46f93f14aeda61314466ea42b5ee0a5d77e12
santosh <santosh.ece06@gmail.com>
2017-10-17 20:22:23 +0530

    autopopulate.

--- src/configs/dbschema.js
+++ src/configs/dbschema.js
@@ -5,12 +5,6 @@ let Schema = mongoose.Schema;
 let Promise = require('bluebird');
 mongoose.Promise = Promise;
 
-
-let autoPopulateTopFollower = function(next) {
-    this.populate('topFollower', 'name');
-    next();
-};
-
 let userSchema = new Schema({
     "name"	        :	{type: String},
     "description"	:	{type: String},
@@ -22,18 +16,7 @@ let userSchema = new Schema({
 
     /* property to test populate */
     "topFollower"   : {type: Schema.Types.ObjectId, ref: "User", autopopulate:{select:'name email'} },
-    "followerList"  : [{type: Schema.Types.ObjectId, ref: "User"}],
-
-    /*"lastFollower" : {
-        "ts" : Date,
-        "detail" : {type: Schema.Types.ObjectId, ref: "User"},
-    },
-
-    "lastFollowerList" : [{
-        "ts" : Date,
-        "detail" : {type: Schema.Types.ObjectId, ref: "User"},
-    }]*/
-}, { timestamps:true});
+}, { collection:"user",  timestamps:true});
 userSchema.plugin(autopopulate);
 
-mongoose.model("User", userSchema, "User");
+mongoose.model("User", userSchema);
